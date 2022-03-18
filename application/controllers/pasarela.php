8deb864ee10d7e4294608614e5b5ea7138480724
OAG HQ <daniel_k310a@hotmail.com>
2022-03-18 15:51:18 -0600

    peticion de tarjeta open

--- application/controllers/pasarela.php
+++ application/controllers/pasarela.php
@@ -27,38 +27,86 @@ class Pasarela extends CI_Controller
 
     public function index()
     {
-       // header('Content-Type: application/json');
+        // header('Content-Type: application/json');
+        Openpay::getProductionMode(false);
         $openpay = Openpay::getInstance('mex0qnhtpq3m0yvkl3sa', 'sk_0d2dbc7f9a6a4f88a5320c75a28815dd', 'MX');
 
-       /* $customerData = array(
-            'name' => 'Teofilo',
-            'last_name' => 'Velazco',
-            'email' => 'teofilo@payments.com',
-            'phone_number' => '4421112233',
-            'address' => array(
-                'line1' => 'Privada Rio No. 12',
-                'line2' => 'Co. El Tintero',
-                'line3' => '',
-                'postal_code' => '76920',
-                'state' => 'Querétaro',
-                'city' => 'Querétaro.',
-                'country_code' => 'MX'));
+        /* $customerData = array(
+        'name' => 'Teofilo',
+        'last_name' => 'Velazco',
+        'email' => 'teofilo@payments.com',
+        'phone_number' => '4421112233',
+        'address' => array(
+        'line1' => 'Privada Rio No. 12',
+        'line2' => 'Co. El Tintero',
+        'line3' => '',
+        'postal_code' => '76920',
+        'state' => 'Querétaro',
+        'city' => 'Querétaro.',
+        'country_code' => 'MX'));
 
         $customer = $openpay->customers->add($customerData);
-        */
+         */
 
         $findData = array(
-            'creation[gte]' => '2022-03-01',
-            'creation[lte]' => '2022-03-31',
+            'creation[gte]' => '2022-01-01',
+            'creation[lte]' => '2022-12-31',
             'offset' => 0,
             'limit' => 5);
 
         $customerList = $openpay->customers->getList($findData);
 
-        $customer = $openpay->customers->get('amhd2eavnlxrobz48wff');
+        // echo json_encode($customerList);
+    }
 
+    public function pagar()
+    {
+        $this->load->view('pasarela');
+    }
 
-        echo json_encode($customer);
-        //Openpay :: getProductionMode(false);
+    public function datosPagos()
+    {
+        header('Content-Type: application/json');
+
+        $openpay = Openpay::getInstance('mex0qnhtpq3m0yvkl3sa', 'sk_0d2dbc7f9a6a4f88a5320c75a28815dd');
+
+        $customer = array(
+            'name' => 'Freud',
+            'last_name' => 'Lamar',
+            'phone_number' => '3312124587',
+            'email' => 'daniel.mora@ctings.com');
+
+        $chargeData = array(
+            'method' => 'card',
+            'source_id' => $_POST["token_id"],
+            'amount' => (float) 5000,
+            'currency' => 'MXN',
+            'description' => "articulo de prueba",
+            'device_session_id' => $_POST["deviceIdHiddenFieldName"],
+            'customer' => $customer,
+        );
+
+    //    $charge = $openpay->charges->create($chargeData);
+
+        echo json_encode(["usuario" => $customer, "datos" => $chargeData]);
+
+    }
+
+    public function codigoEjemplo()
+    {
+        try {
+            $charge = $this->openpay->charges->create($chargeData);
+        } catch (Exception $e) {
+            $errorMsg = $e->getMessage();
+            $errorCode = $e->getCode();
+        }
+        $status = null;
+        if ($errorMsg !== null || $errorCode !== null) {
+            $errorMsg = $this->getError($errorCode);
+            $status = array("status" => false, "error" => $errorMsg, "errorCode" => $errorCode);
+        } else {
+            $status = array("status" => true, "charge" => json_encode($chargeData));
+        }
+        return $status;
     }
 }
---
+++ application/views/pasarela.php
@@ -0,0 +1,112 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+
+    <link href="<?=base_url('src/vendors/openpay/style.css')?>" rel="stylesheet" />
+
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://openpay.s3.amazonaws.com/openpay.v1.min.js"></script>
+    <script type='text/javascript' src="https://openpay.s3.amazonaws.com/openpay-data.v1.min.js"></script>
+
+    <script type="text/javascript">
+    $(document).ready(function() {
+
+        OpenPay.setId('mex0qnhtpq3m0yvkl3sa');
+        OpenPay.setApiKey('pk_1b9a4ef755b8431c91824246ae34f55b');
+        OpenPay.setSandboxMode(true);
+        //Se genera el id de dispositivo
+        var deviceSessionId = OpenPay.deviceData.setup("payment-form", "deviceIdHiddenFieldName");
+
+        $('#pay-button').on('click', function(event) {
+            event.preventDefault();
+            $("#pay-button").prop("disabled", true);
+            OpenPay.token.extractFormAndCreate('payment-form', sucess_callbak, error_callbak);
+        });
+
+        var sucess_callbak = function(response) {
+            var token_id = response.data.id;
+            console.log("Su token es " + token_id);
+            $('#token_id').val(token_id);
+            // $('#payment-form').submit();
+
+            var data = $("#payment-form").serializeArray();
+
+            
+
+            $.ajax({
+                type: 'POST', //aqui puede ser igual get
+                url: './datosPagos', //aqui va tu direccion donde esta tu funcion php
+                data: data, //aqui tus datos
+            });
+        }
+
+        var error_callbak = function(response) {
+            var desc = response.data.description != undefined ? response.data.description : response
+                .message;
+            alert("ERROR [" + response.status + "] " + desc);
+            $("#pay-button").prop("disabled", false);
+        };
+
+    });
+    </script>
+
+
+</head>
+
+<body>
+    <div class="bkng-tb-cntnt">
+        <div class="pymnts">
+            <form action="#" method="POST" id="payment-form">
+                <input type="hidden" name="token_id" id="token_id">
+                <div class="pymnt-itm card active">
+                    <h2>Tarjeta de crédito o débito</h2>
+                    <div class="pymnt-cntnt">
+                        <div class="card-expl">
+                            <div class="credit">
+                                <h4>Tarjetas de crédito</h4>
+                            </div>
+                            <div class="debit">
+                                <h4>Tarjetas de débito</h4>
+                            </div>
+                        </div>
+                        <div class="sctn-row">
+                            <div class="sctn-col l">
+                                <label>Nombre del titular</label><input type="text"
+                                    placeholder="Como aparece en la tarjeta" autocomplete="off"
+                                    data-openpay-card="holder_name">
+                            </div>
+                            <div class="sctn-col">
+                                <label>Número de tarjeta</label><input type="text" autocomplete="off"
+                                    data-openpay-card="card_number">
+                            </div>
+                        </div>
+                        <div class="sctn-row">
+                            <div class="sctn-col l">
+                                <label>Fecha de expiración</label>
+                                <div class="sctn-col half l"><input type="text" placeholder="Mes"
+                                        data-openpay-card="expiration_month"></div>
+                                <div class="sctn-col half l"><input type="text" placeholder="Año"
+                                        data-openpay-card="expiration_year"></div>
+                            </div>
+                            <div class="sctn-col cvv"><label>Código de seguridad</label>
+                                <div class="sctn-col half l"><input type="text" placeholder="3 dígitos"
+                                        autocomplete="off" data-openpay-card="cvv2"></div>
+                            </div>
+                        </div>
+                        <div class="openpay">
+                            <div class="logo">Transacciones realizadas vía:</div>
+                            <div class="shield">Tus pagos se realizan de forma segura con encriptación de 256 bits</div>
+                        </div>
+                        <div class="sctn-row">
+                            <a class="button rght" id="pay-button">Pagar</a>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
